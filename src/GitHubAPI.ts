4d11812e3429d28c3142028304c47055a38ce33d
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-10-10 23:09:56 +1300

    Add basic pagination support

--- src/GitHubAPI.ts
+++ src/GitHubAPI.ts
@@ -16,33 +16,50 @@ class GitHubAPI {
     repoOwner: string,
     repoName: string
   ): Promise<object[]> {
-    try {
-      const response = await axios.get<object[]>(
-        `${GitHubAPI.baseURL}/repos/${repoOwner}/${repoName}/pulls`,
-        {
-          params: {
-            state: 'open',
-          },
-          headers: {
-            Accept: 'application/vnd.github+json',
-          },
-        }
-      );
+    const pullRequests: object[] = [];
 
-      return response.data;
-    } catch (err) {
-      if (isAxiosError(err)) {
-        if (err.response?.status === 404) {
-          throw new Error('Repo not found.');
-        } else {
-          throw new Error(
-            err.response?.data.message || 'An unknown error occurred.'
-          );
+    let page = 1;
+    const perPage = 100;
+    while (true) {
+      try {
+        const response = await axios.get<object[]>(
+          `${GitHubAPI.baseURL}/repos/${repoOwner}/${repoName}/pulls`,
+          {
+            params: {
+              state: 'open',
+              page: page,
+              per_page: perPage,
+            },
+            headers: {
+              Accept: 'application/vnd.github+json',
+            },
+          }
+        );
+
+        pullRequests.push(...response.data);
+
+        if (response.data.length < perPage) {
+          // There are no more pages
+          break;
         }
-      } else {
-        throw new Error('An unknown error occurred.');
+
+        page++;
+      } catch (err) {
+        if (isAxiosError(err)) {
+          if (err.response?.status === 404) {
+            throw new Error('Repo not found.');
+          } else {
+            throw new Error(
+              err.response?.data.message || 'An unknown error occurred.'
+            );
+          }
+        } else {
+          throw new Error('An unknown error occurred.');
+        }
       }
     }
+
+    return pullRequests;
   }
 }
 
